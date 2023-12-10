0c7d61a45540becb0961e50812582fd8a6ca8ca1
cod3kid <djsufail@gmail.com>
2023-12-10 07:24:10 +0530

    Rxjs: Mouse Click Example

--- introduction-ii/index.js
+++ introduction-ii/index.js
@@ -1,4 +1,9 @@
-var Rx = require("rx");
-Rx.Observable.just("Hello World!").subscribe(function (value) {
-  console.log(value);
-});
+const Rx = require("rx");
+Rx.Observable.fromEvent(document, "click")
+  .filter((c) => {
+    return c.clientX > window.innerWidth / 2;
+  })
+  .take(10)
+  .subscribe((c) => {
+    console.log(c.clientX, c.clientY);
+  });
