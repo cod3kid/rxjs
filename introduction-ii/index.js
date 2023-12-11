fb62eb4b4a8aaad97961bb9b0c40a4b14b78e303
cod3kid <djsufail@gmail.com>
2023-12-11 07:27:03 +0530

    Observable with create method

--- introduction-ii/index.js
+++ introduction-ii/index.js
@@ -1,9 +1,22 @@
 const Rx = require("rx");
-Rx.Observable.fromEvent(document, "click")
-  .filter((c) => {
-    return c.clientX > window.innerWidth / 2;
-  })
-  .take(10)
-  .subscribe((c) => {
-    console.log(c.clientX, c.clientY);
-  });
+// Rx.Observable.fromEvent(document, "click")
+//   .filter((c) => {
+//     return c.clientX > window.innerWidth / 2;
+//   })
+//   .take(10)
+//   .subscribe((c) => {
+//     console.log(c.clientX, c.clientY);
+//   });
+
+const observable = Rx.Observable.create((observer) => {
+  observer.onNext("Hello");
+  observer.onNext("Mello");
+  observer.onNext("Kello");
+  observer.onCompleted();
+});
+
+observable.subscribe(
+  (value) => console.log(value),
+  null,
+  () => console.log("Completed")
+);
